2be029b6f7effe96a93dd16853248a1669b8d489
lilu <lilu@126.com>
2016-07-06 23:01:41 +0800

    删除了多余的代码

--- rq_scan/src/main/java/com/bandeng2/lilu/rq_scan/Test1.java
+++
@@ -1,7 +0,0 @@
-package com.bandeng2.lilu.rq_scan;
-
-/**
- * Created by Admin on 2016/6/6.
- */
-public class Test1 {
-}
--- rq_scan/src/main/java/com/bandeng2/lilu/rq_scan/Test2.java
+++
@@ -1,8 +0,0 @@
-package com.bandeng2.lilu.rq_scan;
-
-/**
- * Created by Admin on 2016/6/6.
- */
-public class Test2 {
-	//我是第一个人，测试3
-}
